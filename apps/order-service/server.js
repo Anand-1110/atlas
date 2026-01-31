190b3d9e8b0f2a3a458d24442f98697556085284
Anand-1110 <chaurasia.avinash4002@gmail.com>
2026-01-31 19:53:05 +0530

    Add Prometheus metrics endpoint to order-service and user service

--- apps/order-service/server.js
+++ apps/order-service/server.js
@@ -1,18 +1,45 @@
 require("dotenv").config();
 const express = require("express");
-const { Pool } = require("pg");
+const mongoose = require("mongoose");
+
+// ✅ ADD THIS
+const client = require("prom-client");
 
 const app = express();
 app.use(express.json());
 
-const pool = new Pool({
-  connectionString: process.env.DB_URL
+// ==============================
+// PROMETHEUS METRICS SETUP
+// ==============================
+const register = new client.Registry();
+
+client.collectDefaultMetrics({
+  register,
 });
 
+// expose metrics endpoint
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
+
+// ==============================
+// CONNECT MONGODB
+// ==============================
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// ==============================
+// HEALTH ENDPOINT
+// ==============================
 app.get("/health", (req, res) => {
   res.send("Order Service is running");
 });
 
+// ==============================
+// START SERVER
+// ==============================
 app.listen(5000, () => {
   console.log("Order Service running on port 5000");
 });
